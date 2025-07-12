83d821395448578e63a6493733ef64a94de4fd74
Olajide Nurudeen <84381571+JideJnr@users.noreply.github.com>
2025-07-12 14:00:43 +0100

    commit

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,19 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 console.log('🚦 System initialized...');
-console.log('🚦 Welcome Back !!! ');
+console.log('🚦 Welcome Back !!!');
 console.log('🚦 Standing by for your commands...');
 
-const subscriber = createClient({ url: process.env.REDIS_URL || 'redis://localhost:6379' });
-await subscriber.connect();
+(async () => {
+  const subscriber = createClient({ url: process.env.REDIS_URL || 'redis://localhost:6379' });
+  await subscriber.connect();
 
-subscriber.subscribe('bot-control', (message) => {
-  const { command } = JSON.parse(message);
-  if (command === 'START_BOT') {
-    console.log('🟢 Bot started by API command!');
-  } else if (command === 'STOP_BOT') {
-    console.log('🔴 Bot stopped by API command!');
-  }
-});
+  subscriber.subscribe('bot-control', (message) => {
+    const { command } = JSON.parse(message);
+    if (command === 'START_BOT') {
+      console.log('🟢 Bot started by API command!');
+    } else if (command === 'STOP_BOT') {
+      console.log('🔴 Bot stopped by API command!');
+    }
+  });
+})();
